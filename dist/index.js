8af737ab057bff39aaa90c95ce795aafa0abf9f0
erik <euy@2datafish.com.au>
2017-11-24 16:14:51 +1100

    updated dist code

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ module.exports = {
 	// pick between the local development url or the rawgit hosted 'production' url
 
 	 // url: 'http://localhost:9966/dist/'
-	url: 'https://rawgit.com/erik-uy/Tealium-Environment-Switcher/master/dist/'
+	url: 'https://raw.githubusercontent.com/erik-uy/Tealium-Environment-Switcher/master/dist/'
 };
 },{}],2:[function(require,module,exports){
 'use strict';
