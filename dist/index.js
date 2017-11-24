60cb41ffc11c02b482ae170380d23f422e756d47
erik <euy@2datafish.com.au>
2017-11-24 17:12:53 +1100

    rebuilt to point to rawgit

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,8 @@ module.exports = {
 
 	// pick between the local development url or the rawgit hosted 'production' url
 
-	url: 'http://localhost:9966/dist/'
-	//url: 'https://cdn.rawgit.com/erik-uy/Tealium-Environment-Switcher/master/dist/'
+	//url: 'http://localhost:9966/dist/'
+	url: 'https://cdn.rawgit.com/erik-uy/Tealium-Environment-Switcher/master/dist/'
 };
 },{}],2:[function(require,module,exports){
 'use strict';
--- src/js/config.js
+++ src/js/config.js
@@ -2,6 +2,6 @@ module.exports = {
 
 	// pick between the local development url or the rawgit hosted 'production' url
 
-	url: 'http://localhost:9966/dist/'
-	//url: 'https://cdn.rawgit.com/erik-uy/Tealium-Environment-Switcher/master/dist/'
+	//url: 'http://localhost:9966/dist/'
+	url: 'https://cdn.rawgit.com/erik-uy/Tealium-Environment-Switcher/master/dist/'
 };
